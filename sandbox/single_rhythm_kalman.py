6f55d76bc192c73ef4c1c9ba0c856ef883b0bb1f
nikolaims <n.m.smetanin@gmail.com>
2021-10-21 13:56:08 +0300

    kalman for single rhythm model utils

--- sandbox/single_rhythm_kalman.py
+++ sandbox/single_rhythm_kalman.py
@@ -12,41 +12,62 @@ class SingleRhythmKalman:
         self.tau = tau
         self.fs = fs
 
-        self.x_n_nm1 = np.zeros(2)
-        self.V_n_nm1 = np.eye(2)
-        self.x_n_n = np.zeros(2)
-        self.V_n_n = np.eye(2)
+        self.x_n_nm1 = None
+        self.V_n_nm1 = None
+        self.x_n_n = None
+        self.V_n_n = None
 
         arg = 2*np.pi*f/fs
         self.F = self.a * np.array([[np.cos(arg), -np.sin(arg)],
                                     [np.sin(arg), np.cos(arg)]])
         self.H = np.array([1, 0])
 
+        self.first_step = True
+
     def filter(self, y):
         K = self.V_n_nm1 @ self.H / (self.H @ self.V_n_nm1 @ self.H + self.tau**2)
         self.x_n_n = self.x_n_nm1 + K * (y - self.H @ self.x_n_nm1)
         self.V_n_n = self.V_n_nm1 - K.reshape(-1, 1) @ self.H.reshape(1, -1) @ self.V_n_nm1
-        return self.x_n_n, self.V_n_nm1
+        return self.x_n_n, self.V_n_n
 
     def pred(self):
-        self.x_n_nm1 = self.F @ self.x_n_n
-        self.V_n_nm1 = self.F @ self.V_n_n @ self.F.T + self.sigma**2
+        if not self.first_step:
+            self.x_n_nm1 = self.F @ self.x_n_n
+            self.V_n_nm1 = self.F @ self.V_n_n @ self.F.T + self.sigma**2
+        else:
+            self.x_n_nm1 = np.zeros(2)
+            self.V_n_nm1 = np.eye(2) * self.sigma ** 2 / (1 - self.a ** 2)
+            self.first_step = False
+        return self.x_n_nm1, self.V_n_nm1
 
-from sandbox.single_rhythm_model import SingleRhythmModel
-np.random.seed(42)
-srm = SingleRhythmModel(10, 0.99, 0.1, 2)
-srk = SingleRhythmKalman(10, 0.99, 0.1, 2)
 
-n_steps = FS
-x, y = srm.steps(n_steps)
-x_pred = np.zeros((n_steps, 2))
-V_pred = np.zeros((n_steps, 2, 2))
-for n in range(n_steps):
-    srk.pred()
-    x_pred[n], V_pred[n] = srk.filter(y[n])
+    def collect_states(self, y):
+        n_steps = len(y)
+        x_pred = np.zeros((n_steps, 2))
+        V_pred = np.zeros((n_steps, 2, 2))
+        x_filter = np.zeros((n_steps, 2))
+        V_filter = np.zeros((n_steps, 2, 2))
+        for n in range(n_steps):
+            x_pred[n], V_pred[n] = self.pred()
+            x_filter[n], V_filter[n] = self.filter(y[n])
+        return x_pred, V_pred, x_filter, V_filter
 
-t = np.arange(n_steps)/FS
 
-plt.plot(t, x_pred[:, 0])
-plt.fill_between(t, x_pred[:, 0]-2*V_pred[:, 0, 0]**0.5, x_pred[:, 0]+2*V_pred[:, 0, 0]**0.5, alpha=0.5)
-plt.plot(t, x[:, 0])
+if __name__ == '__main__':
+    from sandbox.single_rhythm_model import SingleRhythmModel
+    np.random.seed(42)
+    srm = SingleRhythmModel(10, 0.99, 0.1, 2)
+    srk = SingleRhythmKalman(10, 0.99, 0.1, 2)
+
+    n_steps = FS
+    x, y = srm.steps(n_steps)
+
+    _x_pred, _V_pred , x_filter, V_filter = srk.collect_states(y)
+
+    t = np.arange(n_steps)/FS
+
+    plt.plot(t, x_filter[:, 0])
+    plt.fill_between(t, x_filter[:, 0]-2*V_filter[:, 0, 0]**0.5, x_filter[:, 0]+2*V_filter[:, 0, 0]**0.5, alpha=0.5)
+    plt.plot(t, x[:, 0])
+
+    # tau_hat = ((y - srk.H @ x_pred)**2/(srk.H.repeat(500).reshape(2, 500) @ V_pred @ srk.H + 1)).sum()
